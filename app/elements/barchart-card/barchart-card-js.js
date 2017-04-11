72b7d485cd678dbfce3e66652dc1b1ee563a7fc6
srgpriyankaa <srg.priyankaa@gmail.com>
2017-04-11 17:38:09 +0530

    Add files via upload

---
+++ app/elements/barchart-card/barchart-card-js.js
@@ -0,0 +1,24 @@
+(function() {
+  'use strict';
+Polymer({
+  is: "barchart-card",
+  barchartloading:function(e){
+      alert(JSON.stringify(e[0]));
+      document.getElementById("chartContainer").innerHTML = "barchart is loaded.";
+      var dataPoints = [];
+      for(var key in e[0]){
+        dataPoints.push({label: key, y: parseInt(e[0][key])});
+    }
+      var chart = new CanvasJS.Chart("chartContainer",{
+        title: {
+          text: "ItemStockDetails"
+        },
+        data: [{
+          type: "column",
+          dataPoints: dataPoints
+        }]
+      });
+      chart.render();
+    }
+});
+})();
