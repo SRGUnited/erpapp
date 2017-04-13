ebd63d4b2f0b28d1d9ba74a3f8aaa667d233937f
srgPrasanna <srg.prasanna@gmail.com>
2017-04-13 18:41:07 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,15 +3,10 @@ var mysql      = require('mysql');
 var bodyParser = require('body-parser');
 var jsonfile   = require('jsonfile');
 
-var fetchpath=[];
-var fetchpath = "./app/config/fetch.json";
-
-var file='/tmp1/data.json';
-
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var connection;
-  require('fs').readFile('/polymer/erpapp-current/app/config/getconnection.json','utf8',function(err,data){
+  require('fs').readFile('/polymer/erpapp/app/config/getconnection.json','utf8',function(err,data){
     dbjson=JSON.parse(data);
     global.connection=mysql.createConnection({
       host:dbjson[0].host,
@@ -41,10 +36,10 @@ app.post('/login', urlencodedParser, function (req, res) {
     emp_id:req.query.empid,
     password:req.query.password
   };
-  	global.connection.query("SELECT * FROM test.emp_login where emp_id='"+req.query.empid+"' and password='"+req.query.password+"'",function(err,rows){
+  	global.connection.query("SELECT * FROM emp_login where emp_id='"+req.query.empid+"' and password='"+req.query.password+"'",function(err,rows){
   	if(rows.length>0){
       var roleid=rows[0].role_id;
-    global.connection.query("select * from emp_login_menu where menu_id in(SELECT menu_id FROM test.menu_map where role_id='"+roleid+"')",function(err,rows){
+    global.connection.query("select * from emp_login_menu where menu_id in(SELECT menu_id FROM menu_map where role_id='"+roleid+"')",function(err,rows){
         res.status(200).json({'returnval': rows});
       });
       }
@@ -54,70 +49,9 @@ app.post('/login', urlencodedParser, function (req, res) {
     });
 });
 
-var salessummarydb=require("./app/elements/salesorder-summary/salessummarydb.js");
-salessummarydb.getconnection();
-app.get('/fetch', urlencodedParser, function (req, res) {
-salessummarydb.fetch(function(rows){
-    if(rows!="reject"){
-      res.status(200).json({'returnval': rows});
-    }
-    else
-      res.status(200).json({'returnval': "Invalid!"});
-  });
-});
-
-var vehiclesummarydb=require("./app/elements/vehicleorder-summary/vehiclesummarydb.js");
-vehiclesummarydb.getconnection();
-app.post('/vehiclefetch', urlencodedParser, function (req, res) {
-  vehiclesummarydb.vehiclefetch(function(rows){
-    if(rows!="reject"){
-      res.status(200).json({'returnval': rows});
-    }
-    else
-      res.status(200).json({'returnval': "Invalid!"});
-  });
-});
-
-var itemdesigndb=require("./app/elements/all-design/itemdesigndb.js");
-itemdesigndb.getconnection();
-app.post('/itemfetch', urlencodedParser, function (req, res) {
-  itemdesigndb.itemfetch(function(rows){
-    if(rows!="reject"){
-      res.status(200).json({'returnval': rows});
-    }
-    else
-      res.status(200).json({'returnval': "Invalid!"});
-  });
-});
-
-var customerdesigndb=require("./app/elements/all-design/customerdesigndb.js");
-customerdesigndb.getconnection();
-app.post('/customerfetch', urlencodedParser, function (req, res) {
-  customerdesigndb.customerfetch(function(rows){
-    if(rows!="reject"){
-      res.status(200).json({'returnval': rows});
-    }
-    else
-      res.status(200).json({'returnval': "Invalid!"});
-  });
-});
-
-var timelinedb=require("./app/elements/timeline-chart/timelinedb.js");
-timelinedb.getconnection();
-app.post('/timelinefetch', urlencodedParser, function (req, res) {
-  timelinedb.timelinefetch(function(rows){
-    if(rows!="reject"){
-      res.status(200).json({'returnval': rows});
-    }
-    else
-      res.status(200).json({'returnval': "Invalid!"});
-  });
-});
-
-
 var itemdetailsdb=require("./app/elements/item-details/item-details-todb.js");
 app.post('/insertitems', urlencodedParser, function (req, res) { //add items save process
-  itemdetailsdb.insertitems(req.query.id,req.query.name,req.query.description,req.query.specification1,req.query.specification2,req.query.container,req.query.unit,req.query.group,req.query.type,req.query.status,req.query.ptype,function(callback){
+  itemdetailsdb.insertitems(req.query.sid,req.query.id,req.query.name,req.query.description,req.query.specification1,req.query.specification2,req.query.container,req.query.unit,req.query.group,req.query.type,req.query.status,req.query.ptype,req.query.iprice,req.query.ceostatus,function(callback){
     if(callback=="saved!"){
       res.status(200).json({'returnval': "Saved!"});
     }
@@ -128,18 +62,76 @@ app.post('/insertitems', urlencodedParser, function (req, res) { //add items sav
 });
 
 app.post('/searchitem', urlencodedParser, function (req, res) { //add item search process
-  itemdetailsdb.searchitem(req.query.id,function(rows){
-    if(rows!="no data")
-      res.status(200).json({'returnval': rows});
+  itemdetailsdb.searchitem(req.query.name,function(callback){
+    // console.log("callback"+JSON.stringify(callback));
+    if(callback!=null)
+      res.status(200).json({'returnval':callback});
     else
-      res.status(200).json({'returnval': "Data not found!"});
+      res.status(200).json({'returnval':"No Data"});
   });
 });
 
-
+var salespersondb=require("./app/elements/sales-person/sales-person-todb.js");
 app.post('/insertsales', urlencodedParser, function (req, res) {
-  connectdb.insertsales(req.query.datetimeq,req.query.rcoilsq,req.query.rtonq,req.query.status,req.query.datetimeq1,function(callback){
+  salespersondb.insertsales(req.query.salesid,req.query.datetimeq,req.query.ccname,req.query.customerid,req.query.customerlocation,req.query.id,req.query.description,req.query.ispecification,req.query.rcoilsq,req.query.rtonq,req.query.rdqty,req.query.datetimeq1,req.query.status,function(callback){
+    if(callback==saved)
+    res.status(200).json({'returnval':"saved"});
+    else {
+    res.status(200).json({'returnval':"not saved"});
+    }
   })
+});
+
+var itemapprovaldb=require("./app/elements/call-ceo-card/call-ceo-card-todb.js")
+app.post('/ceoitemsearch', urlencodedParser, function (req, res) { //add item search process
+  itemapprovaldb.searchitem(function(callback){
+    if(callback=="Data found"){
+      console.log("Written in json");
+      res.status(200).json({'returnval': "Data found"});
+    }
+    else{
+      console.log("Can't write in json!");
+      // res.status(200).json({'returnval': "Unable to save!"});
+    }
+  });
+});
+
+app.post('/ceoresponse', urlencodedParser, function (req, res) { //add item search process
+  console.log(req.query.respond);
+  itemapprovaldb.ceoresponse(req.query.respond,req.query.itemid,function(callback){
+    if(callback=="Data found"){
+      console.log("Written in json");
+      res.status(200).json({'returnval': "Data found"});
+    }
+    else{
+      console.log("Can't write in json!");
+      // res.status(200).json({'returnval': "Unable to save!"});
+    }
+  });
+});
+
+app.post('/mapitem', urlencodedParser, function (req, res) { //add item search process
+  global.connection.query("SELECT itemname FROM m_item_details",function(err,rows){
+    console.log("itemname"+JSON.stringify(rows));
+  if(rows.length>0)
+  res.status(200).json({'returnval': rows});
+  else
+    res.status(200).json({'returnval': "Invalid!"});
+  });
+});
+
+app.post('/mapsupplier', urlencodedParser, function (req, res) { //add item search process
+  global.connection.query("SELECT suppliername FROM m_supplierdetails",function(err,rows){
+    console.log("supname"+JSON.stringify(rows));
+  if(rows.length>0)
+  res.status(200).json({'returnval': rows});
+  else
+    res.status(200).json({'returnval': "Invalid!"});
+  });
+});
+
+app.post('/fixsupplier', urlencodedParser, function (req, res) {
+    global.connection.query("",function(err,supid){});
 });
 
 app.listen(4000);
