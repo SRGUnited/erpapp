5eda661cc04a4ecb070fc8a751c08a9937204974
SRGsaswathac <srg.saswathac@gmail.com>
2017-04-15 12:25:09 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var jsonfile   = require('jsonfile');
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var connection;
+var connectdb=require("./app/script/connectdb.js");
   require('fs').readFile('/polymer/erpapp/app/config/getconnection.json','utf8',function(err,data){
     dbjson=JSON.parse(data);
     global.connection=mysql.createConnection({
@@ -143,5 +144,483 @@ app.post('/fixsupplier', urlencodedParser, function (req, res) {
     }
   });
 });
+// slider server file
+
+var salessummarydb=require("./app/elements/salesorder-summary/salessummarydb.js");
+// salessummarydb.getconnection();
+app.get('/fetch', urlencodedParser, function (req, res) {
+salessummarydb.fetch(function(rows){
+    if(rows!="reject"){
+      res.status(200).json({'returnval': rows});
+    }
+    else
+      res.status(200).json({'returnval': "Invalid!"});
+  });
+});
+
+var timelinedb=require("./app/elements/timeline-chart/timelinedb.js");
+app.post('/timelinefetch', urlencodedParser, function (req, res) {
+  timelinedb.timelinefetch(req.query.salesid,req.query.itemssid,function(rows){
+    if(rows!="reject"){
+      res.status(200).json({'returnval': rows});
+    }
+    else
+      res.status(200).json({'returnval': "Invalid!"});
+  });
+});
+
+var sliderfetchdb=require("./app/elements/slider-barnew/sliderdb.js");
+app.post('/sliderchange', urlencodedParser,function (req, res) {
+  console.log("sliderrows");
+  sliderfetchdb.sliderchange(req.query.itemssid,function(rows,callback){
+    if(rows!="reject"){
+      res.status(200).json({'returnval': rows});
+    }
+    else{
+      res.status(200).json({'returnval': "Invalid!"});
+    }
+  });
+});
+// vehicle     7777777777777777
+app.post('/securityinfo', urlencodedParser, function (req, res) {
+  connectdb.securityjsonsaveFn(req.query.invNum,req.query.saleid,req.query.invDate,req.query.delqunty,
+    req.query.vehouttime,function(err,rows){
+    if(rows="json writed"){
+      // console.log("not reject");
+      res.status(200).json({'returnval': rows});
+    }
+    else
+      res.status(200).json({'returnval': "Json not writed"});
+  });
+  });
+
+  app.post('/supplieritempostinfo', urlencodedParser, function (req,res) {
+    var response={supplierid:req.query.supid,
+                  itemid:req.query.itemid};
+      connectdb.supplieritempostFn(response,function(rows){
+        if(rows=="saved"){
+          res.status(200).json({'returnval': rows});
+        }
+        else
+          res.status(200).json({'returnval': "not saved"});
+      });
+      });
+      app.post('/customeritempostinfo', urlencodedParser, function (req,res) {
+        var response={customerid:req.query.supid,
+                      itemid:req.query.itemid};
+          connectdb.customeritempostFn(response,function(rows){
+            if(rows=="saved"){
+              res.status(200).json({'returnval': rows});
+            }
+            else
+              res.status(200).json({'returnval': "not saved"});
+          });
+          });
+      app.post('/searchsupplierid', urlencodedParser, function (req,res) {
+        var response={supname:req.query.supname}
+        connectdb.searchsupplieridFn(response,function(rows){
+          if(rows!="not get"){
+        if(rows.length>0){
+          res.status(200).json({'returnval': rows});
+              }
+            }
+        else
+          res.status(200).json({'returnval': "does not get supplier details"});
+          });
+      });
+
+
+      app.post('/searchcustomerid', urlencodedParser, function (req,res) {
+        var response={supname:req.query.supname}
+        connectdb.searchcustomeridFn(response,function(rows){
+          if(rows!="not get"){
+        if(rows.length>0){
+          res.status(200).json({'returnval': rows});
+              }
+            }
+        else
+          res.status(200).json({'returnval': "does not get supplier details"});
+          });
+      });
+
+// app.post('/getsupplierdata', urlencodedParser, function (req,res) {
+//   connectdb.getsupplierdataFn(function(rows){
+//     if(rows!="not get"){
+//   if(rows.length>0){
+//     res.status(200).json({'returnval': rows});
+//         }
+//       }
+//   else
+//     res.status(200).json({'returnval': "does not get supplier details"});
+//     });
+//  });
+app.post('/loopsecuritysearchinfo', urlencodedParser, function (req,res) {
+connectdb.loopsecuritysearchFn(req.query.salid,function(rows){
+  if(rows!="reject"){
+    res.status(200).json({'returnval': rows});
+  }
+  else
+    res.status(200).json({'returnval': "does not get saleid"});
+});
+});
+app.post('/supplieritem_map', urlencodedParser, function (req,res) {
+  connectdb.supplieritem_mapFn(function(rows){
+    if(rows.length>0){
+      res.status(200).json({'returnval': rows});
+    }
+    else
+      res.status(200).json({'returnval': "didn't get value"});
+  });
+  });
+  app.post('/customeritem_map', urlencodedParser, function (req,res) {
+    connectdb.customeritem_mapFn(function(rows){
+      if(rows.length>0){
+        res.status(200).json({'returnval': rows});
+      }
+      else
+        res.status(200).json({'returnval': "didn't get value"});
+    });
+    });
+app.post('/autosecuritysearchinfo', urlencodedParser, function (req,res) {
+  connectdb.autosecuritysearchFn(function(rows){
+    if(rows!="reject"){
+      // console.log(rows);
+      res.status(200).json({'returnval': rows});
+    }
+    else
+      res.status(200).json({'returnval': "does not get vehicleno"});
+  });
+  });
+  app.post('/securitysearchinfo', urlencodedParser, function (req,res) {
+    connectdb.securitysearchFn(req.query.searchvehiclenum,function(rows){
+      if(rows!="reject"){
+        // console.log(rows);
+        res.status(200).json({'returnval': rows});
+      }
+      else
+        res.status(200).json({'returnval': "does not get vehicel no"});
+    });
+    });
+app.post('/insertitems', urlencodedParser, function (req, res) {
+var response={
+  itemid:req.query.id,
+  itemname:req.query.name,
+  itemdescription:req.query.description,
+  container:req.query.container,
+  // Item_Unit:req.query.unit,
+  // Item_Type:req.query.type,
+  // Item_Group:req.query.group,
+  status:req.query.purchase
+};
+connection.query('INSERT INTO m_itemdetail SET ?',[response],function(err,result){
+  if(result.affectedRows>0)
+    res.status(200).json({'returnval': "Saved!"});
+    // jsonfile.writeFile(file,response,function(err){
+    // console.error("Error:"+err)
+          // })
+  else
+      res.status(200).json({'returnval': "Unable to save!"});
+  });
+});
+
+app.post('/ceocustomerapprovalinfo', urlencodedParser, function (req, res) {
+// console.log("ceo-customer-card");
+var response={customerid:req.query.cuid,
+              status:req.query.radio};
+connectdb.ceocustomeapprovalFn(response,function(rows){
+  if(rows!="reject"){
+    res.status(200).json({'returnval': rows});
+  }
+  else
+    res.status(200).json({'returnval': "customer table doesn't updated"});
+});
+});
+
+
+app.post('/ceocustomerajaxinfo', urlencodedParser, function (req, res) {
+// console.log("ceo-customer-card");
+connectdb.ceocustomerajaxFn(function(rows){
+  if(rows!="reject"){
+    res.status(200).json({'returnval': rows});
+  }
+  else
+    res.status(200).json({'returnval': "ceo-customer-card.json not created"});
+});
+});
+app.post('/vehiclefetch', urlencodedParser, function (req, res) {
+connectdb.vehiclefetch(function(rows){
+  if(rows!="reject"){
+    res.status(200).json({'returnval': rows});
+  }
+  else
+    res.status(200).json({'returnval': "vehicle.json not created"});
+});
+});
+app.post('/searchsalesorder', urlencodedParser, function (req, res) {
+connectdb.searchsalesorderconncetdbFn(function(rows){
+  if(rows!="reject"){
+    // console.log(rows);
+    res.status(200).json({'returnval': rows});
+  }
+  else
+    res.status(200).json({'returnval': "Invalid salesordercreate id"});
+});
+});
+// app.post('/autocomplete', urlencodedParser, function (req,res) {
+//   // console.log("autocomplte");
+//   	connection.query("select customerid from m_customerdetail",function(err,rows){
+//   	if(!err)
+//       res.status(200).json({'datavalue': rows});
+//     else{
+//       // console.log(err);
+//       res.status(200).json({'datavalue': "autocomplete access"});
+//     }
+//     });
+// });
+
+
+// var vehiclepath=[];
+// var vehiclepath = "./app/config/vehicle.json";
+// app.post('/vehiclesummary',urlencodedParser,function (req, res) {
+//
+//     connection.query('Select * FROM vehicle_summary',function(err,rows){
+//     if(rows.length>0){
+//     res.status(200).json({'returnval': rows});
+//
+//     jsonfile.writeFile(vehiclepath,rows,function(err){
+//
+//     })
+//   }
+//   else{
+//     res.status(200).json({'returnval': "Data not found!"});
+//   }
+//   });
+// });
+
+
+
+app.post('/vehsavedata', urlencodedParser, function (req, res) {
+var response={
+  vehnoval:req.query.VehicleNoval,
+  drivnameval:req.query.DriverNameval,
+  drivmobnoval1:req.query.DriverMobNumberval1,
+  drivmobnoval2:req.query.DriverMobNumberval2,
+  ownmobnumberval:req.query.OwnMobNumberval,
+  vehtimeval:req.query.Vehintimeval,
+  vehtime2val:req.query.Vehoutimeval,
+  vehdateval:req.query.Vehdateval
+};
+
+connection.query('INSERT INTO vehicle_table SET ?',[response],function(err,result){
+  if(!err)
+    res.status(200).json({'datavalue': "Saved!"});
+    // jsonfile.writeFile(file,response,function(err){
+    // console.error("Error:"+err)
+
+  else
+      res.status(200).json({'datavalue': "Unable to save!"});
+  });
+});
+
+
+app.post('/insertsales', urlencodedParser, function (req, res) {
+var response={
+
+  // Cust_name:req.query.ccname,
+  // Item_ID:req.query.id,
+  // Item_Name:req.query.name,
+  // Item_Spec:req.query.ispec,
+  // Item_Description:req.query.description,
+  salesorderdate:req.query.datetimeq,
+  containerquantity:req.query.rcoilsq,
+  orderquantity:req.query.rtonq,
+  status:req.query.status,
+  requiredeliverydate:req.query.datetimeq1
+
+
+
+
+};
+// connectdb.insertsales();
+
+connectdb.insertsales(req.query.datetimeq,req.query.rcoilsq,req.query.rtonq,req.query.status,req.query.datetimeq1,function(callback){
+
+})
+  // connection.query('INSERT INTO test.salesordercreate SET ?',[response],function(err,result){
+
+
+  // if(result.affectedRows>0)
+  // jsonfile.writeFile('../config/person.json', response, function (err) {
+  //  console.error(err)
+//  })
+// res.status(200).json({'returnval': "Saved!"});
+
+
+
+  // else
+    // res.status(200).json({'returnval': "Unable to save!"});
+  // });
+});
+
+
+
+app.post('/searchitem', urlencodedParser, function (req, res) {
+var response={
+  Item_ID:req.query.id
+};
+  connection.query('Select * FROM itemdetails_card WHERE ?',[response],function(err,rows){
+  if(rows.length>0)
+    res.status(200).json({'returnval': rows});
+  else{
+    res.status(200).json({'returnval': "Data not found!"});
+  }
+  });
+});
+
+// var connection=null;
+// app.post('/loadconnection' ,function (req, res) {
+//   dbname=req.query.dbname;
+//   dbpass=req.query.dbpass;
+//   dbuser=req.query.dbuser;
+//   dbport=req.query.dbport;
+//   dbhost=req.query.dbhost;
+//   connection = mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'admin',
+//     database:'test'
+//     });
+//  res.status(200).json({'returnval': "Connected"});
+// });
+
+app.post('/savedata', urlencodedParser, function (req, res) {
+var response={
+  customerid:req.query.sidval,
+  customername:req.query.snameval,
+  address1:req.query.adval1,
+  address2:req.query.adval2,
+  address3:req.query.adval3,
+  city:req.query.citynameval,
+  state:req.query.stateVal,
+  country:req.query.countryVal,
+  pincode:req.query.pinval,
+  mobile1:req.query.mobnumval1,
+  mobile2:req.query.mobnumval2,
+  email:req.query.emidval,
+  status:req.query.statusval
+};
+connectdb.savecustomertdetFn(response,function(result){
+  if(result=="saved"){
+    res.status(200).json({'datavalue': "Saved!"});
+  }
+  else {
+    res.status(200).json({'datavalue': "Saved!"});
+  }
+});
+// var connection = mysql.createConnection({
+//    host    : req.query.dbhost,
+//    user    : req.query.dbuser,
+//   password : req.query.dbpass,
+//    database: req.query.dbname
+//  });
+
+});
+app.post('/savesupplierdata', urlencodedParser, function (req, res) {
+var response={
+  supplierid:req.query.sidval,
+  suppliername:req.query.snameval,
+  address1:req.query.adval1,
+  address2:req.query.adval2,
+  address3:req.query.adval3,
+  city:req.query.citynameval,
+  state:req.query.stateVal,
+  country:req.query.countryVal,
+  pincode:req.query.pinval,
+  mobile1:req.query.mobnumval1,
+  mobile2:req.query.mobnumval2,
+  email:req.query.emidval,
+  status:req.query.statusval
+};
+console.log(response);
+connectdb.savesupplierdetFn(response,function(result){
+  if(result=="saved"){
+    res.status(200).json({'datavalue': "Saved!"});
+  }
+  else {
+    res.status(200).json({'datavalue': "Saved!"});
+  }
+});
+// var connection = mysql.createConnection({
+//    host    : req.query.dbhost,
+//    user    : req.query.dbuser,
+//   password : req.query.dbpass,
+//    database: req.query.dbname
+//  });
+
+});
+
+app.post('/Taxsaveinfo', urlencodedParser, function (req, res) {
+var response={
+  pan:req.query.pan_no,
+  tann:req.query.tann_no,
+  cin:req.query.cin_no,
+  tin:req.query.tin_no,
+  cst:req.query.cst_no
+  };
+connection.query('INSERT INTO tax_card SET ?',[response],function(err,result){
+  if(result.affectedRows>0)
+    res.status(200).json({'returnval': "Saved!"});
+
+  else
+      res.status(200).json({'returnval': "Unable to save!"});
+  });
+});
+
+app.post('/searchtax', urlencodedParser, function (req, res) {
+var response={
+  pan:req.query.pan_no
+};
+  connection.query('Select * FROM tax_card WHERE ?',[response],function(err,rows){
+  if(rows.length>0)
+    res.status(200).json({'returnval': rows});
+  else{
+    res.status(200).json({'returnval': "Data not found!"});
+  }
+  });
+});
+
+app.post('/saveexcise', urlencodedParser, function (req, res) {
+var response={
+  cexregno:req.query.value1,
+  eccno:req.query.value2,
+  range:req.query.value3,
+  division:req.query.value4,
+  excise_cardcol:req.query.value5,
+  servicetaxno:req.query.value6
+  };
+connection.query('INSERT INTO excise_card SET ?',[response],function(err,result){
+  if(result.affectedRows>0)
+    res.status(200).json({'returnval': "Saved!"});
+
+  else
+      res.status(200).json({'returnval': "Unable to save!"});
+  });
+});
+app.post('/savesalesvehicleorder', urlencodedParser, function(req,res) {
+var response={goodsvehiclenumber:req.query.vehno,
+              salesorderid:req.query.salid
+              };
+  connectdb.savesalesorderconncetdbFn(response,function(data){
+  if(rows="saved"){
+    // console.log(data);
+  res.status(200).json({'returnval': "data saved"});
+}
+  else{
+    // console.log(err);
+  res.status(200).json({'returnval': "data not saved"});
+}
+});
+});
+
 
 app.listen(4000);
