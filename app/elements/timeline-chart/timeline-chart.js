bca4593f9e69acdb81a2e4e7ed7be2abd4c305ac
srgpriyankaa <srg.priyankaa@gmail.com>
2017-04-22 19:21:48 +0530

    Add files via upload

--- app/elements/timeline-chart/timeline-chart.js
+++ app/elements/timeline-chart/timeline-chart.js
@@ -2,7 +2,14 @@
     'use strict';
  Polymer({
       is: 'timeline-chart',
-    
+//       mouseover:function(e){
+//         // document.addEventListener('mouseover', function (e) {
+//         var time = document.getElementById('timeline-badge');
+//         time.time = "active";
+//         false;
+//
+// },
+
     	    getJsondata:function(goodsvehiclenumber,goodsvehicleintime,loadstart,loadend,goodsvehicleouttime,delivered){
     			  	this.vehicleno=goodsvehiclenumber;
           	  this.vehicleintime=goodsvehicleintime;
